89e7b589a8ea84d5286fb861e019b9973047234e
Alex <exndiver@gmail.com>
2019-04-13 19:35:16 +0200

    fix layout

--- rec/src/components/Header.vue
+++ rec/src/components/Header.vue
@@ -1,15 +1,27 @@
 <template>
 <v-toolbar>
-  <v-toolbar-title>Reciples!</v-toolbar-title>
+  <v-toolbar-title @click="navigate({name: 'Main'})" class="main_page">Reciples!</v-toolbar-title>
+  <v-spacer></v-spacer>
+  <v-toolbar-items>
+    <v-btn flat @click="navigate({name: 'Register'})">
+     Sign Up
+    </v-btn>
+  </v-toolbar-items>
 </v-toolbar>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    navigate (route) {
+      this.$router.push(route)
+    }
+  }
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.main_page{
+  cursor: pointer;
+}
 </style>
